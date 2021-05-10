b8bf11acff5aee6444b6d08a694549b34e51fd65
Wojtek <wrzesien.ww@gmail.com>
2021-05-10 21:30:18 +0200

    algorithm with list data structure

--- twitter.py
+++ twitter.py
@@ -2,24 +2,12 @@ import pickle
 import logging
 import tweepy
 
-# log = logging.getLogger("twitter")
-# log.setLevel(logging.DEBUG)
-# ch = logging.StreamHandler()
-# ch.setLevel(logging.NOTSET)
-
-# formatter = logging.Formatter('%(message)s')
-# ch.setFormatter(formatter)
-# log.addHandler(ch)
-
 log = logging.getLogger("tns")
 log.setLevel(logging.DEBUG)
 log.propagate = False
 
 # Twitter API credentials
-CONSUMER_KEY = "SOmoMXIHBGv1af7MSJ2ws2cag"
-CONSUMER_SECRET = "3oTmNzSOuBJUzLiL2yk8SCGF5il7cnEE57Jchk5uMJONb2gtR6"
-ACCESS_TOKEN = "1388823775551107083-hqQn9objB2tRrkqZIVHgcnjmLGJGBy"
-ACCESS_TOKEN_SECRET = "bwZUFsqcocAuZGKg4RrTnB9mXZW0tS7e9vZ6A5eqHxkGD"
+
 
 class TwitterDatabase(object):
     def __init__(self, hashtag, tweets_num):
@@ -41,12 +29,17 @@ class TwitterDatabase(object):
             api.search, 
             q=self.hashtag,
             lang="en",
-            tweet_mode='extended'
+            # tweet_mode='extended'
         ).items(self.tweets_num)
-        self.database_words = [tweet.full_text.lower().split() for tweet in tweets]
+        for tweet in tweets:
+            data = []
+            if len(tweet.entities['hashtags']) != 0:
+                for h in tweet.entities['hashtags']:
+                    data.append(h['text'].lower())
+                self.database_words.append(data)
 
         log.debug(self.database_words[0])
-        save = {'data': self.database_words, 'min_item': self.min_item, 'max_item': self.max_item}
+        save = {'data': self.database_words, 'min_item': self.min_item, 'max_item': self.max_item, 'mapping':self.map_to_words}
         self.save_pickle(save, "data_words.pickle")
 
     def mapping(self):
@@ -61,22 +54,22 @@ class TwitterDatabase(object):
                 if word not in self.map_to_words.values():
                     self.map_to_words[self.max_item] = word
                     temp[word] = self.max_item
+                    self.max_item += 1
                 seq_new.append([temp[word]])
-                self.max_item += 1
             self.database.append(seq_new)
 
         log.debug(self.database[0])
-        save = {'data': self.database, 'min_item': self.min_item, 'max_item': self.max_item}
+        save = {'data': self.database, 'min_item': self.min_item, 'max_item': self.max_item, 'mapping':  self.map_to_words}
         self.save_pickle(save, "data_int.pickle")
     
     def print_stats(self, results):
         """Display final stats"""
         for result in results:
-            i_words = [self.map_to_words[num] for num in result.i]
-            j_words = [self.map_to_words[num] for num in result.j]
+            i_words = [self.map_to_words[num] for num in result["i"]]
+            j_words = [self.map_to_words[num] for num in result["j"]]
 
             string = "{} -> {}, sup: {}, conf: {}".format(
-                i_words, j_words, result.sup, result.conf
+                i_words, j_words, result["sup"], result["conf"]
             )
             log.info(string)
 
@@ -92,18 +85,18 @@ class TwitterDatabase(object):
             data = pickle.load(file)
         self.min_item = data['min_item']
         self.max_item = data['max_item']
+        self.map_to_words = data['mapping']
         if 'int' in filename:
             self.database = data['data']
         else:
             self.database_words = data['data']
 
 if __name__ == "__main__":
-    twr = TwitterDatabase("covid", 100)
-    # twr.retrieve_tweets()
-    twr.load_pickle("./data_words.pickle", twr.database_words)
+    twr = TwitterDatabase("covid", 10)
+    twr.retrieve_tweets()
+    # twr.load_pickle("./data_words.pickle", twr.database_words)
     print(twr.database_words)
     twr.mapping()
-    # print(twr.database)
 
 
 
