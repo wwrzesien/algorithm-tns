549c28f1c2c4b9f2844ffa7f1c92b9aabed801ca
Wojtek <wrzesien.ww@gmail.com>
2021-05-26 18:49:48 +0200

    finished ERMiner algorithm

--- ERMiner/expand_left_store.py
+++ ERMiner/expand_left_store.py
@@ -1,4 +1,7 @@
 import logging
+from typing import List, Set, Dict
+
+from ERMiner.left_equivalence import LeftEquivalence
 
 log = logging.getLogger("erminer")
 log.setLevel(logging.DEBUG)
@@ -8,5 +11,25 @@ log.propagate = False
 class ExpandLeftStore(object):
     """LeftStore structure used by the ERMiner algorithm"""
     def __init__(self):
-        super().__init__()
-        pass
+        self.store = {}
+
+    def register(self, leftRule, itemset_j: List, tids_j: Set[int], occurs_i: Dict, occurs_j:Dict):
+        size = len(itemset_j)
+        hash_itemset_j = hash(tuple(itemset_j))
+
+        if size not in self.store.keys():
+            self.store[size] = {}
+        
+        if hash_itemset_j not in self.store[size].keys():
+            eclass = LeftEquivalence(itemset_j, tids_j, occurs_j)
+            self.store[size][hash_itemset_j] = [eclass]
+            eclass.rules.append(leftRule)
+        else:
+            for eclass in self.store[size][hash_itemset_j]:
+                if eclass.itemset_j == itemset_j:
+                    eclass.rules.append(leftRule)
+                    return
+            eclass = LeftEquivalence(itemset_j, tids_j, occurs_j)
+            self.store[size][hash_itemset_j].append(eclass)
+            eclass.rules.append(leftRule)
+
--- main_er.py
+++ main_er.py
@@ -1,7 +1,9 @@
 import logging
 import tweepy
+
 from ERMiner.erminer_alg import AlgorithmERMiner
-# from twitter import TwitterDatabase
+
+from twitter import TwitterDatabase
 
 # Create logger
 log = logging.getLogger("erminer")
@@ -30,17 +32,34 @@ TEST_DB = [
 if __name__ == "__main__":
     log.info("Start ERMiner algorithm")
 
-    min_supp = 3
-    min_conf = 5
+    min_supp = 15
+    min_conf = 0.5
     class Database(object):
         def __init__(self, *args):
             self.database = TEST_DB
             self.min_item = 1
             self.max_item = 7
 
-    db = Database()
+    # twr = Database()
 
-    erminer = AlgorithmERMiner(database=db, min_conf=min_conf, min_supp=min_supp)
+    twr = TwitterDatabase('photography', 1000)
+    # twr.retrieve_tweets()
+    twr.load_pickle("./data_words.pickle")
+    twr.mapping()
+    # twr.load_pickle("./data_int.pickle")
+    # twr.save_pickle(twr.map_to_words, "./map_to_words.pickle")
 
-    # erminer.calc_tidset_ij_and_ji({}, 3, ["d"], {1})
+    erminer = AlgorithmERMiner(database=twr, min_conf=min_conf, min_supp=min_supp)
+
+    erminer.run_algorithm()
+    results = erminer.get_results()
+
+    log.info("")
+    log.info("------- ERMiner stats --------")
+    log.info("Minsup: {}".format(min_supp))
+    log.info("Sequential rules count: {}".format(len(results)))
+    log.info("")
+    
+    # erminer.print_stats()
+    twr.print_stats(results)
 
