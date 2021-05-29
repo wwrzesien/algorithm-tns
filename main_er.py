a981e5ffad9e9916adbd75591019f82d9a8bcaa6
Wojtek <wrzesien.ww@gmail.com>
2021-05-29 12:59:23 +0200

    test execution time

--- main_er.py
+++ main_er.py
@@ -1,6 +1,7 @@
 import logging
 import tweepy
 import time
+import pickle
 
 from ERMiner.erminer_alg import AlgorithmERMiner
 
@@ -36,6 +37,52 @@ class Database(object):
         self.min_item = 1
         self.max_item = 7
 
+def test_algorithm(min_conf):
+    # min_supp = [1, ]
+    files = {"1000": "data_words_1000.pickle", "1500": "data_words_1500.pickle", "3000": "data_words_3000.pickle", "comb": "data_words_combined.pickle"}
+    min_supp = [1, 5, 10, 15, 20]
+
+    r = {"comb": [], "1000": [], "1500": [], "3000": [], "supp": min_supp}
+
+    twr = TwitterDatabase('photography', 300)
+    for key, file in files.items():
+        for supp in min_supp:
+            twr.load_pickle("./" + file)
+            twr.mapping("data_int.pickle")
+
+            erminer = AlgorithmERMiner(database=twr, min_conf=min_conf, min_supp=supp)
+
+            start_time = time.time()
+            erminer.run_algorithm()
+            exec_time = round(time.time() - start_time, 4)
+            log.info("Execution time: {}s".format(exec_time))
+            
+            results = erminer.get_results()
+
+            log.info("")
+            log.info("------- ERMiner stats --------")
+            log.info("Minsup: {}".format(supp))
+            log.info("Sequential rules count: {}".format(len(results)))
+            log.info("")
+            
+            twr.print_stats(results)
+            r[key].append(exec_time)
+
+
+    with open("results.pickle", 'wb') as file:
+        pickle.dump(r, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def combine_datasets():
+    files = ["data_words_500.pickle", "data_words_1000.pickle", "data_words_1500.pickle", "data_words_3000.pickle"]
+    twr = TwitterDatabase('photography', 300)
+    combine_db = []
+    for file in files:
+        twr.load_pickle("./" + file)
+        combine_db += twr.database_words
+    twr.database_words = combine_db
+    save = {'data': twr.database_words, 'min_item': twr.min_item, 'max_item': twr.max_item, 'mapping':twr.map_to_words, "stats": twr.stats}
+    twr.save_pickle(save, "data_words_combined.pickle")
 
 if __name__ == "__main__":
     log.info("Start ERMiner algorithm")
@@ -43,31 +90,34 @@ if __name__ == "__main__":
     min_supp = 15
     min_conf = 0.5
 
+    # combine_datasets()
+    test_algorithm(min_conf)
+
     # test database
     # twr = Database()
 
     # analitical database
-    twr = TwitterDatabase('photography', 1000)
-    # twr.retrieve_tweets("data_words_1000.pickle")
-    twr.load_pickle("./data_words_1000.pickle")
-    twr.mapping()
-    # twr.load_pickle("./data_int.pickle")
-    # twr.save_pickle(twr.map_to_words, "./map_to_words.pickle")
+    # twr = TwitterDatabase('photography', 3000)
+    # twr.retrieve_tweets("data_words_3000.pickle")
+    # twr.load_pickle("./data_words_1000.pickle")
+    # twr.mapping()
+    # # twr.load_pickle("./data_int.pickle")
+    # # twr.save_pickle(twr.map_to_words, "./map_to_words.pickle")
 
-    erminer = AlgorithmERMiner(database=twr, min_conf=min_conf, min_supp=min_supp)
+    # erminer = AlgorithmERMiner(database=twr, min_conf=min_conf, min_supp=min_supp)
 
-    start_time = time.time()
-    erminer.run_algorithm()
-    log.info("Execution time: {}s".format(round(time.time() - start_time, 4)))
+    # start_time = time.time()
+    # erminer.run_algorithm()
+    # log.info("Execution time: {}s".format(round(time.time() - start_time, 4)))
 
-    results = erminer.get_results()
+    # results = erminer.get_results()
 
-    log.info("")
-    log.info("------- ERMiner stats --------")
-    log.info("Minsup: {}".format(min_supp))
-    log.info("Sequential rules count: {}".format(len(results)))
-    log.info("")
+    # log.info("")
+    # log.info("------- ERMiner stats --------")
+    # log.info("Minsup: {}".format(min_supp))
+    # log.info("Sequential rules count: {}".format(len(results)))
+    # log.info("")
     
-    # erminer.print_stats()
-    twr.print_stats(results)
+    # # erminer.print_stats()
+    # twr.print_stats(results)
 
--- twitter.py
+++ twitter.py
@@ -1,13 +1,14 @@
 import pickle
 import logging
 import tweepy
+import numpy as np
 
-log = logging.getLogger("tns")
+# log = logging.getLogger("tns")
+log = logging.getLogger("erminer")
 log.setLevel(logging.DEBUG)
 log.propagate = False
 
 # Twitter API credentials
-
 
 class TwitterDatabase(object):
     def __init__(self, hashtag, tweets_num):
@@ -18,8 +19,10 @@ class TwitterDatabase(object):
         self.database_words = []
         self.min_item = 1
         self.max_item = 1
+
+        self.stats = {"seq_count": 0, "dist_item_count": 0, "avg_seq_len": []}
     
-    def retrieve_tweets(self):
+    def retrieve_tweets(self, filename):
         """Call Twitter API, create words database"""
         auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
         auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -39,13 +42,15 @@ class TwitterDatabase(object):
                 self.database_words.append(data)
 
         log.debug(self.database_words[0])
-        save = {'data': self.database_words, 'min_item': self.min_item, 'max_item': self.max_item, 'mapping':self.map_to_words}
-        self.save_pickle(save, "data_words.pickle")
+        save = {'data': self.database_words, 'min_item': self.min_item, 'max_item': self.max_item, 'mapping':self.map_to_words, "stats": self.stats}
+        self.save_pickle(save, filename)
 
-    def mapping(self):
+    def mapping(self, filename):
         """Create map int-word and int database"""
         temp = {}
         for seq in self.database_words:
+            self.stats["seq_count"] += 1
+            self.stats["avg_seq_len"].append(len(seq))
             seq_new = []
             for word in seq:
                 if "@" in word or (word is seq[-1] and '...' in word):
@@ -58,12 +63,19 @@ class TwitterDatabase(object):
                 seq_new.append([temp[word]])
             self.database.append(seq_new)
 
+        self.stats['avg_seq_len'] = np.mean(self.stats['avg_seq_len'])
+        self.stats['dist_item_count'] = len(self.map_to_words)
         log.debug(self.database[0])
-        save = {'data': self.database, 'min_item': self.min_item, 'max_item': self.max_item, 'mapping':  self.map_to_words}
-        self.save_pickle(save, "data_int.pickle")
+        save = {'data': self.database, 'min_item': self.min_item, 'max_item': self.max_item, 'mapping':  self.map_to_words, "stats": self.stats}
+        self.save_pickle(save, filename)
     
     def print_stats(self, results):
         """Display final stats"""
+
+        log.info("Sequence count: {}".format(self.stats["seq_count"]))
+        log.info("Distinct item count: {}".format(self.stats['dist_item_count']))
+        log.info("Avg sequence length: {}".format(self.stats['avg_seq_len']))
+        log.info("")
         for result in results:
             i_words = [self.map_to_words[num] for num in result["i"]]
             j_words = [self.map_to_words[num] for num in result["j"]]
@@ -71,7 +83,7 @@ class TwitterDatabase(object):
             string = "{} -> {}, sup: {}, conf: {}".format(
                 i_words, j_words, result["sup"], result["conf"]
             )
-            log.info(string)
+            # log.info(string)
 
     def save_pickle(self, data, filename):
         """Save data to picke"""
@@ -81,11 +93,13 @@ class TwitterDatabase(object):
 
     def load_pickle(self, filename):
         """Load data from pickle"""
+        log.info("Load: {}".format(filename))
         with open(filename, "rb") as file:
             data = pickle.load(file)
         self.min_item = data['min_item']
         self.max_item = data['max_item']
         self.map_to_words = data['mapping']
+        self.stats = data["stats"]
         if 'int' in filename:
             self.database = data['data']
         else:
